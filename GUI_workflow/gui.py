9957c60654e128d0a8f4ffb2ef0a1d11e635df1d
Meghan Brandt <mbrandt5@vols.utk.edu>
2024-05-01 12:26:55 -0400

    Adding loading display during model generation

--- GUI_workflow/gui.py
+++ GUI_workflow/gui.py
@@ -2,41 +2,40 @@ import tkinter as tk
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-# import itertools
-# import threading
-# import time
-# import sys
-# 
+import itertools
+import threading
+import time
+import sys
+
 # done = False
-# #here is the animation
 # def animate():
 #     for c in itertools.cycle(['|', '/', '-', '\\']):
 #         if done:
 #             break
-#         sys.stdout.write('\rloading ' + c)
-#         sys.stdout.flush()
-#         time.sleep(0.1)
-#     sys.stdout.write('\rDone!     ')
-# 
-# t = threading.Thread(target=animate)
-# t.start()
-# 
-# #long process here
-# time.sleep(10)
-# done = True
+       
+#         load.config(text=c)
+#         root.update()
 
-# tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True)
-# #add .cuda() for gpu
-# model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True)
-# input_text = "add two numbers in fortran"
-# inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
-# outputs = model.generate(**inputs, max_length=128)
-# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+#         time.sleep(0.15)
 
-######################################
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# import torch
+# def animation(process) :
+#     while process.is_alive():
+#         chars = ['|', '/', '-', '\\']
+#         for c in chars:
+#             load.config(text=c)
+#             time.sleep(0.15)
 
+# tester function TODO: remove
+def long_running_computation(target_time):
+    start_time = time.time()
+    i = 1
+    result = 0
+    while time.time() - start_time < target_time:
+        result += i**2
+        i += 1
+    return result
+
+# model generation
 def model_generate(user_prompt):
     # generate model result using prompt
     checkpoint = "smallcloudai/Refact-1_6B-fim"
@@ -53,12 +52,10 @@ def model_generate(user_prompt):
 
     inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
     outputs = model.generate(inputs, max_length=1000, temperature=0.2)
-    # print("-"*80)
     return tokenizer.decode(outputs[0])
-######################################
 
 
-######################################  * GUI *
+# filter output from model
 def filter_in(str):
     result = ""
     for line in str.splitlines():
@@ -69,12 +66,45 @@ def filter_in(str):
     
     return result
 
+# clear textbox containing model results
 def clear_output():
     output_text.config(state="normal")
     output_text.delete('1.0', tk.END)
     output_text.config(state="disabled")
 
-def get_input(event=None):
+# helper function for loading and generating model results
+def loading(event=None):
+    load.config(text="Loading . . .")
+    # time.sleep(0.5)
+    root.update()
+    get_input()
+
+# second helper function (used when 'Enter' submits user prompt)
+def loading2(event):
+    load.config(text="Loading . . .")
+    # time.sleep(0.5)
+    root.update()
+    get_input()
+
+
+# function connecting user submission with model generation
+def get_input():
+    # done = False
+    # a = threading.Thread(target=animate, daemon=False)
+    # a.start()
+    # root.update()
+    
+
+    # animation(the_process)
+    # animate()
+    # the_process_function()
+    # the_process.join()
+    # long_running_computation(3)
+    # time.sleep(3)
+    # done = True
+    # output_text.insert(tk.END, "end of computation") 
+    
+
     user_input = entry.get()
     # output_text.delete('1.0', tk.END)
     output_text.config(state="normal")
@@ -82,26 +112,24 @@ def get_input(event=None):
     model_result = model_generate(user_input)
     output_text.insert(tk.END, filter_in(model_result) + "\n\n\n\n\n\n\n\n")
 
-    # output_text.insert(tk.END, user_input + "\n\n\n\n\n\n\n\n") #### tmp uncomment for only gui testing
-    output_text.yview(tk.END)
+    load.config(text=" ")
+    root.update()
+    #long process here
+    # entry.delete('1.0', tk.END)
 
-    output_text.config(state="disabled")
-    entry.delete(0, tk.END)  # Clear the entry box after submitting
-
-def get_input_ret(event):
-    user_input = entry.get()
-    # output_text.delete('1.0', tk.END)
-    output_text.config(state="normal")
-    output_text.insert(tk.END, user_input + "\n")
-    model_result = model_generate(user_input)
-    output_text.insert(tk.END, filter_in(model_result) + "\n\n\n\n\n\n\n\n")
+    # the_process_function()
+    # time.sleep(10)
+    done = True
 
     # output_text.insert(tk.END, user_input + "\n\n\n\n\n\n\n\n") #### tmp uncomment for only gui testing
     output_text.yview(tk.END)
 
     output_text.config(state="disabled")
     entry.delete(0, tk.END)  # Clear the entry box after submitting
+    load.config(text=" ")
 
+
+# * GUI * #
 root = tk.Tk()
 root.title("Fortran Code Generator")
 
@@ -115,14 +143,26 @@ label.pack(side=tk.LEFT)
 
 entry = tk.Entry(input_frame, width=100, font=('Times New Roman', 20))
 entry.pack(side=tk.LEFT)
-entry.bind('<Return>', get_input_ret)
+entry.bind('<Return>', loading2)
 
 # Create a frame for the button
-button_frame = tk.Frame(root)
-button_frame.pack(pady=5)
+h_frame = tk.Frame(root)
+h_frame.pack(pady=5)
+
+load_frame = tk.Frame(h_frame, padx=10)
+load_frame.grid(row=0, column=0)
+
+button_frame = tk.Frame(h_frame)
+button_frame.grid(row=0, column=1)
+
+empty_frame = tk.Frame(h_frame, width=100)
+empty_frame.grid(row=0, column=3)
+
+load = tk.Label(load_frame, text=" ", width=10, font=('Times New Roman', 20))
+load.pack(side=tk.LEFT)
 
 # Button widget for submitting input
-submit = tk.Button(button_frame, text="Submit", font=('Times New Roman', 20), command=get_input)
+submit = tk.Button(button_frame, text="Submit", font=('Times New Roman', 20), command=loading)
 submit.pack(side=tk.LEFT)
 
 clear = tk.Button(button_frame, text="Clear", font=('Times New Roman', 20), command=clear_output)
