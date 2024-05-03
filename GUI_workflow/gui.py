05fd76135d02c42da548ef228731e4a519beaa29
Meghan Brandt <mbrandt5@vols.utk.edu>
2024-05-02 23:01:19 -0400

    final edits to GUI

--- GUI_workflow/gui.py
+++ GUI_workflow/gui.py
@@ -2,38 +2,6 @@ import tkinter as tk
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-import itertools
-import threading
-import time
-import sys
-
-# done = False
-# def animate():
-#     for c in itertools.cycle(['|', '/', '-', '\\']):
-#         if done:
-#             break
-       
-#         load.config(text=c)
-#         root.update()
-
-#         time.sleep(0.15)
-
-# def animation(process) :
-#     while process.is_alive():
-#         chars = ['|', '/', '-', '\\']
-#         for c in chars:
-#             load.config(text=c)
-#             time.sleep(0.15)
-
-# tester function TODO: remove
-def long_running_computation(target_time):
-    start_time = time.time()
-    i = 1
-    result = 0
-    while time.time() - start_time < target_time:
-        result += i**2
-        i += 1
-    return result
 
 # model generation
 def model_generate(user_prompt):
@@ -75,38 +43,19 @@ def clear_output():
 # helper function for loading and generating model results
 def loading(event=None):
     load.config(text="Loading . . .")
-    # time.sleep(0.5)
     root.update()
     get_input()
 
 # second helper function (used when 'Enter' submits user prompt)
 def loading2(event):
     load.config(text="Loading . . .")
-    # time.sleep(0.5)
     root.update()
     get_input()
 
 
 # function connecting user submission with model generation
 def get_input():
-    # done = False
-    # a = threading.Thread(target=animate, daemon=False)
-    # a.start()
-    # root.update()
-    
-
-    # animation(the_process)
-    # animate()
-    # the_process_function()
-    # the_process.join()
-    # long_running_computation(3)
-    # time.sleep(3)
-    # done = True
-    # output_text.insert(tk.END, "end of computation") 
-    
-
     user_input = entry.get()
-    # output_text.delete('1.0', tk.END)
     output_text.config(state="normal")
     output_text.insert(tk.END, user_input + "\n")
     model_result = model_generate(user_input)
@@ -114,16 +63,8 @@ def get_input():
 
     load.config(text=" ")
     root.update()
-    #long process here
-    # entry.delete('1.0', tk.END)
 
-    # the_process_function()
-    # time.sleep(10)
-    done = True
-
-    # output_text.insert(tk.END, user_input + "\n\n\n\n\n\n\n\n") #### tmp uncomment for only gui testing
     output_text.yview(tk.END)
-
     output_text.config(state="disabled")
     entry.delete(0, tk.END)  # Clear the entry box after submitting
     load.config(text=" ")
@@ -166,7 +107,7 @@ submit = tk.Button(button_frame, text="Submit", font=('Times New Roman', 20), co
 submit.pack(side=tk.LEFT)
 
 clear = tk.Button(button_frame, text="Clear", font=('Times New Roman', 20), command=clear_output)
-clear.pack(side=tk.RIGHT, padx=25)
+clear.pack(side=tk.RIGHT, padx=15)
 
 # Create a Text widget for displaying output
 output_text = tk.Text(root, width=200, height=80, font=('Times New Roman', 20))
